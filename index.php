56ae3c2b1e4147718543f829fd482f1c80e21a28
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-04-17 16:45:28 +0200

    [add] header

--- index.php
+++ index.php
@@ -10,27 +10,40 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js" integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
   <!-- Css -->
-  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="assets/css/style.css" />
   <title>PHP Dischi JSON</title>
 </head>
 
 <body>
   <div id="app">
-    <div class="container my-5 text-center">
-      <h1 class="text-white">{{ title }}</h1>
-      <div class="d-flex flex-wrap justify-content-center">
-        <div v-for="(item,index) in lista_disc" :key="index" class="card m-4 d-flex justify-content-center align-items-center" style="width: 18rem">
-          <img :src="item.poster" class="card-img-top p-3" alt="item.poster" />
-          <div class="card-body text-white">
-            <h5 class="card-title">{{ item.title }}</h5>
-            <h6 class="card-subtitle mb-2">{{ item.author }}</h6>
-            <h6 class="card-subtitle mb-2">{{ item.year }}</h6>
-            <p class="card-text">{{ item.genre }}</p>
-          </div>
+    <header class="shadow">
+      <div class="container h-100">
+        <div class="logo">
+          <img src="assets/img/spotify.png" alt="">
         </div>
       </div>
+    </header>
 
-    </div>
+
+    <main>
+      <div class="container my-5 text-center">
+
+        <h1 class="text-white">{{ title }}</h1>
+        <div class="d-flex flex-wrap justify-content-center">
+          <div v-for="(item,index) in lista_disc" :key="index" class="card m-4 d-flex justify-content-center align-items-center" style="width: 18rem">
+            <img :src="item.poster" class="card-img-top p-3" alt="item.poster" />
+            <div class="card-body text-white">
+              <h5 class="card-title">{{ item.title }}</h5>
+              <h6 class="card-subtitle mb-2">{{ item.author }}</h6>
+              <h6 class="card-subtitle mb-2">{{ item.year }}</h6>
+              <p class="card-text">{{ item.genre }}</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </main>
+
   </div>
 
   <script src="js/script.js"></script>
